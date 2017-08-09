9b2914572ff48ed2c4d547e896132c4438c8f95f
Alessandro Aleotti <71196@studenti.unimore.it>
2017-08-09 12:25:35 +0200

    modifiche VM. mail di conferma eliminata

--- AWD/settings.py
+++ AWD/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+8*(m2o%w_n9@pb5ed2+yusu5x@oru8dtev*y41)6y92e1=h!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- AWD_Zanasi/urls.py
+++ AWD_Zanasi/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url(r'^$', views.index, name='index'),
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^projects/create_project/$', views.create_project, name='create_project'),
     url(r'^projects/(?P<project_id>[0-9]+)/results/$', views.project_results, name='project_results'),
     url(r'^projects/(?P<project_id>[0-9]+)/edit/$', views.edit_project, name='edit_project'),
